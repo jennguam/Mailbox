6736f2399a416ba63e69764a7deeb9e897fd8c5d
Jenn Leung <jleung@jleung.local>
2016-02-18 22:44:46 -0800

    got panning semi working

--- Mailbox/MailboxViewController.swift
+++ Mailbox/MailboxViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class MailboxViewController: UIViewController {
     
+    @IBOutlet var msg: UIImageView!
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var msgView: UIView!
+    var msgOriginalCenter: CGPoint!
+//    var msgOffset: CGFloat!
+//    var msgLeft: CGPoint!
+//    var msgRight: CGPoint!
+    @IBOutlet var underMsg: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.contentSize = imageView.image!.size
+        var panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "didPanMsg:")
+        
+        msgView.userInteractionEnabled = true
+        msgView.addGestureRecognizer(panGestureRecognizer)
         // Do any additional setup after loading the view.
     }
 
@@ -23,16 +35,62 @@ class MailboxViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    @IBAction func didPanMsg(sender:
+        UIPanGestureRecognizer) {
+            var point = sender.locationInView(msgView)
+            var velocity = sender.velocityInView(msgView)
+            var translation = sender.translationInView(msgView)
+            
+            
+            if sender.state == UIGestureRecognizerState.Began {
+                msgOriginalCenter = msgView.center
+                //print("Gesture began at: \(point)")
+            } else if sender.state == UIGestureRecognizerState.Changed {
+                //print("Gesture changed at: \(point)")
+                if velocity.y > 0 {
+                    if translation.x >= 60 {
+                        underMsg.backgroundColor = UIColor(red: 0.9804, green: 0.8275, blue: 0.2, alpha: 1.0)
+                    } else if translation.x >= 260 {
+                        underMsg.backgroundColor = UIColor(red: 0.8471, green: 0.651, blue: 0.4588, alpha: 1.0)
+                    }
+                    
+                    UIView.animateWithDuration(0, delay: 0, options:[] , animations: { () -> Void in
+                            self.msgView.center = CGPoint(x: self.msgOriginalCenter.x + translation.x, y: self.msgOriginalCenter.y)
+                            print(translation)
+                        }, completion: { (Bool) -> Void in
+                    })
+                    msgView.addSubview(underMsg)
+                    print("Gesture went right")
+                }
+                else {
+                    print("Gesture went left")
+                    if translation.x >= 60 {
+                        underMsg.backgroundColor = UIColor(red: 0.4392, green: 0.851, blue: 0.3843, alpha: 1.0)
+                    } else if translation.x >= 260 {
+                        underMsg.backgroundColor = UIColor(red: 0.9216, green: 0.3294, blue: 0.2, alpha: 1.0)
+                    }
+                    
+                    UIView.animateWithDuration(0, delay: 0, options:[] , animations: { () -> Void in
+                        self.msgView.center = CGPoint(x: self.msgOriginalCenter.x + translation.x, y: self.msgOriginalCenter.y)
+                        print(translation)
+                        }, completion: { (Bool) -> Void in
+                    })
+                }
+               
+                
+            } else if sender.state == UIGestureRecognizerState.Ended {
+                //print("Gesture ended at: \(point)")
+                
+                    UIView.animateWithDuration(0.4, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] , animations: { () -> Void in
+                        self.msgView.center = CGPoint(x: self.msgOriginalCenter.x, y: self.msgOriginalCenter.y)
+                        }, completion: { (Bool) -> Void in
+                    })
 
-    /*
-    // MARK: - Navigation
+                
+            
+            }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
